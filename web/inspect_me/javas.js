3252bd4589e346499dc8397448b0784dfa461f36
tron <37307225+cyber7ron@users.noreply.github.com>
2021-06-29 02:23:25 -0400

    Add files via upload
    web chall

---
+++ web/inspect_me/javas.js
@@ -0,0 +1,8 @@
+var secretCode = "X01lZWVffQ==";
+document.getElementsByTagName('button').onclick=function()  {
+	click();
+}
+function click() {
+	document.getElementById("he").innerHTML = "You Can't find me...";
+}
+   
